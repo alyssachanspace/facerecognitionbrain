071c6d9e8c4c28b8972c3ecfb24afa6e9a09b65e
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-06 15:36:53 +0800

    create register form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 // import Particles from 'react-particles-js';
 import Navigation from './components/Navigation';
-import Signin from './components/Signin'
+import Signin from './components/Signin';
+import Register from './components/Register';
 import Logo from './components/Logo';
-import Rank from './components/Rank'
-import ImageLinkForm from './components/ImageLinkForm'
-import FaceRecognition from './components/FaceRecognition'
+import Rank from './components/Rank';
+import ImageLinkForm from './components/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition';
 import './App.css';
 
 // const particlesOptions = {
@@ -32,7 +33,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -50,7 +52,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box)
     this.setState({box: box})
   }
 
@@ -69,29 +70,39 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
     this.setState({ route: route })
   }
 
   render() {
+    const { isSignedIn, imageUrl, box, route } = this.state
     return (
       <div className="App">
         {/* <Particles 
           className='particles'
           params={particlesOptions} /> */}
         <div className='flex justify-between flex-reverse mt3'>
-          <Navigation onRouteChange={this.onRouteChange} />
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
           <Logo />
         </div>
-        { this.state.route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange} />
-          : <div>
+        { route === 'home'
+          ? <div>
               <Rank />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
+          : (
+              route === 'signin' 
+              ? <Signin onRouteChange={this.onRouteChange} /> 
+              : <Register onRouteChange={this.onRouteChange} />
+            )
         }
       </div>
     );
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Navigation = ({ onRouteChange }) => {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
   return (
     <nav className='flex justify-end'>
-      <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        {isSignedIn ? <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p> : (
+          <p className='f3 link dim black pa3 pointer'>
+            <span onClick={() => onRouteChange('signin')} className='underline'>Sign In</span> / <span onClick={() => onRouteChange('register')} className='underline'>Register</span>
+          </p>
+        )}
+      
     </nav>
   )
 }
