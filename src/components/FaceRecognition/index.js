9eb76ccd272e6fb3f1c213ce4b5c5cffb60dd999
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-06 15:45:51 +0800

    adjust image spacing on mobile

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -4,7 +4,7 @@ import './FaceRecognition.css'
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='flex justify-center ma'>
-      <div className='flex justify-center items-start mt2 mb4 absolute'>
+      <div className='flex justify-center items-start mt2 mb4 relative w-90'>
         <img id='inputImage' src={imageUrl} alt='' className='mw-90 h-auto' style={{ width: '500px' }} />
         <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
       </div>
