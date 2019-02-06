314d818f1e52bc708dfe3a0984182bae55ac8bf2
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-06 12:03:00 +0800

    install react-particles-js for background animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import Rank from './components/Rank'
 import ImageLinkForm from './components/ImageLinkForm'
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          className='particles'
+          params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
