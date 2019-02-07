61f989d4c2494fc3fac197c99f0923da3df13351
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 18:27:15 +0800

    update heroku server

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
 
   onImageSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://tranquil-inlet-50338.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -84,7 +84,7 @@ class App extends Component {
       .then(res => res.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://tranquil-inlet-50338.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
