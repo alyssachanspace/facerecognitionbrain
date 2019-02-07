f342e107def32f96c9a9459aa74575f4ff60b2e2
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 16:51:09 +0800

    reject empty field for registration

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -34,9 +34,11 @@ class Register extends React.Component {
     })
       .then(res => res.json())
       .then(user => {
-        if (user) {
+        if (user.id) {
           this.props.loadUser(user)
           this.props.onRouteChange('home')
+        } else {
+          alert('Incorrect form submission')
         }
       })
   }
