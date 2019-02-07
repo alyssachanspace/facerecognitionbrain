ad94d9477c5a58c370aee64d3d33a81aa2e624db
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 10:45:34 +0800

    modify image container

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -3,9 +3,9 @@ import './FaceRecognition.css'
 
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className='flex justify-center ma'>
-      <div className='flex justify-center items-start mt2 mb4 relative w-90'>
-        <img id='inputImage' src={imageUrl} alt='' className='mw-90 h-auto' style={{ width: '500px' }} />
+    <div className='flex justify-center ma w-90 center'>
+      <div className='flex justify-center items-start mt2 mb4 absolute'>
+        <img id='inputImage' src={imageUrl} alt='' className='mw-100 h-auto' style={{ width: '500px' }} />
         <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
       </div>
     </div>
